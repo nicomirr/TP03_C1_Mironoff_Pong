e65fba1f6d26f1bbfc4d40218f17ceab2923069e
Nicolas Mironoff <156594752+nicomirr@users.noreply.github.com>
2026-09-04 13:06:50 -0300

    Incremento de velocidad de pelota al chocar con paletas

--- Assets/Scripts/Ball/BallMovement.cs
+++ Assets/Scripts/Ball/BallMovement.cs
@@ -1,11 +1,17 @@
 using Game.Ball;
 using UnityEngine;
 using System.Collections;
+using Game.Player;
 
 public class BallMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _increaseAmout = 0.2f;
+
+    [SerializeField] private float _initialSpeed = 10f;
+    [SerializeField] private float _maxSpeed = 16f;
     [SerializeField] private float _minHorizontalDirection = 0.6f;
+
+    private float _currentSpeed;
 
     private BallLauncher _ballLauncher;
     private Rigidbody2D _rb;
@@ -20,6 +26,8 @@ public class BallMovement : MonoBehaviour
 
     private IEnumerator Start()
     {
+        _currentSpeed = _initialSpeed;
+
         yield return _ballLauncher.LaunchRoutine();
 
         _isLaunched = true;
@@ -36,6 +44,16 @@ public class BallMovement : MonoBehaviour
             direction.x = Mathf.Sign(direction.x) * _minHorizontalDirection;
         }
 
-        _rb.linearVelocity = direction.normalized * _speed;
+        _rb.linearVelocity = direction.normalized * _currentSpeed;
+    }
+
+    //Moverlo a otro lugar
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<PlayerController>(out PlayerController player))
+        {
+            if (_currentSpeed < _maxSpeed)
+                _currentSpeed = Mathf.Clamp(_currentSpeed + _increaseAmout, _initialSpeed, _maxSpeed);
+        }
     }
 }
